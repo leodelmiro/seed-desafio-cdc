bf3b69db8637b2a1ef43cea85676c900e5209857
Leonardo Delmiro <leodelmiroms@gmail.com>
2023-08-16 14:03:25 -0300

    feat: initial commit

---
+++ src/main/kotlin/com/leodelmiro/casadocodigokotlin/CasaDoCodigoApplication.kt
@@ -0,0 +1,11 @@
+package com.leodelmiro.casadocodigokotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CasaDoCodigoApplication
+
+fun main(args: Array<String>) {
+	runApplication<CasaDoCodigoApplication>(*args)
+}
---
+++ src/test/kotlin/com/leodelmiro/casadocodigokotlin/CasaDoCodigoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.leodelmiro.casadocodigokotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CasaDoCodigoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
